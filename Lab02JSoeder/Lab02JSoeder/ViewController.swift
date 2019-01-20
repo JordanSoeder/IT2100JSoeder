d11a40890e16eb4c44cc7a21c9c0635c12fe8808
Jordan Soeder <S0001934@acad.tri-c.edu>
2019-01-20 15:34:07 -0500

    Created Lab02JSoeder Project

---
+++ Lab02JSoeder/Lab02JSoeder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab02JSoeder
+//
+//  Created by Jordan Soeder on 1/20/19.
+//  Copyright © 2019 Jordan Soeder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
