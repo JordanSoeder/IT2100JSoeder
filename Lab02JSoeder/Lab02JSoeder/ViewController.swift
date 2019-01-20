1e7ae70cef90025e0ec50d8fbe53ab23a78e41db
Jordan Soeder <S0001934@acad.tri-c.edu>
2019-01-20 16:06:07 -0500

    Created Lab02JSoeder Project

--- Lab02JSoeder/Lab02JSoeder/ViewController.swift
+++ Lab02JSoeder/Lab02JSoeder/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myMessage: UILabel
+        myMessage=UILabel(frame:CGRect(x:30.0,y:50.0,width:300.0,height:50.0))
+        myMessage.font=UIFont.systemFont(ofSize: 48.0)
+        myMessage.text="Hello Xcode"
+        myMessage.textColor=UIColor(patternImage: UIImage(named: "Background")!)
+        view.addSubview(myMessage)
+        NSLog("Hello XCode, Again")
+        
+    }
+    
+    // MARK: - Method for handling memory management.
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
